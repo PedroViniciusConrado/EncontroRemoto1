74e28acf50567be57dd9864ce6c7f145fdade726
PedroViniciusConrado <pvc.programacao@gmail.com>
2021-12-11 04:58:01 -0300

    Segundo commit

--- CadastroPecas.js
+++ CadastroPecas.js
@@ -1,24 +1,36 @@
-let listaPeca = ["Filtro de Ar", "Motor", "Amortecedor"];
+console.log (" \n\BEM-VINDO AO SISTEMA DE CADASTRO DE PEÇAS!\n\ ");
 
-if (listaPeca.length < 10){
-    //É necessário cadastrar
-    console.log ("É possível cadastrar mais peças.");
-} else {
-    console.log ("Não tem mais espaço na lista.");
+const entrada = require ('prompt-sync')({sigint: true});
+let listaPeca = [];
+let resposta = "sim";
+
+while (listaPeca.length < 10 && resposta.toLowerCase() == "sim") {
+    resposta = entrada ("Digite o nome da peça: ");
+    let nomePeca = resposta;
+
+    if (nomePeca.length > 3){
+        resposta = entrada ("Qual o peso da peça (em gramas)? ");
+        let pesoPeca = resposta.replace(/[^0-9]/g,'');
+
+        if (pesoPeca > 100) {
+            let adicionar = listaPeca.push (nomePeca);
+            console.log (listaPeca);
+
+        } else {
+            console.log ("Cada peça DEVE ter MAIS de 100 gramas!");
+        }
+
+    }else {
+        console.log ("Cada nome de peça DEVE ter MAIS de 3 letras!");
+    }
+    resposta = entrada ("Quer cadastrar outra peça? ");
 }
 
+if (listaPeca.length < 10) {
+    console.log (" \n\Essa é sua lista de peças:\n\ ");
+    console.log (listaPeca);
 
-let peso = 50;
-if (peso < 100){
-    console.log ("A peça deve pesar no mínimo 100 g.");
 } else {
-    console.log ("A peça possui peso adequado.");
-}
-
-
-let nomePeca = "Disco de freio";
-if (nomePeca.length > 3){
-    console.log ("Nome de peça está adequado para o cadastro!");
-} else {
-    console.log ("O nome deve ter mais de 3 caracteres, digite um nome adequado!");
+    console.log (" \n\SUA LISTA TERMINOU POR TER ATINGIDO O NÚMERO MÁXIMO DE PEÇAS (10)! Eis a sua lista:\n\ ");
+    console.log (listaPeca);
 }
